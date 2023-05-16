4d0016e506b8c17d36b7296b48a49e6f0a1c7b06
NetCinematics <alpinefalcon@protonmail.com>
2023-05-15 21:18:36 -0700

    prompt2

--- src/components/AiCall2.js
+++ src/components/AiCall2.js
@@ -3,26 +3,38 @@ import axios from 'axios'
 
 const AiCall2 = () => {
   const [responses, setResponses] = useState(null)
+  const [prompt, setPrompt] = useState('how are you?')
   useEffect( () => {
+    sendAiMSG();
+  }, [])
+
+  function sendAiMSG(){
+    if(!prompt)return;
     const options = {
-        method: 'GET',
-        url: 'https://node-dashboard-server.vercel.app/ai2',
-        // url: 'http://localhost:8008/ai2',
-        params: {prompt:'what time is it?'}
+      method: 'GET',
+      url: 'https://node-dashboard-server.vercel.app/ai2',
+      params: {'prompt':prompt}
+      // url: 'http://localhost:8008/ai2',
+      // params: {prompt:'what time is it?'}
     }
     axios.request(options).then((response) => {
         setResponses(response.data)
     }).catch((error) => {
         console.error(error)
-    })
-  }, [])
+    })    
+  }
 
+  function handleClick(){
+    sendAiMSG();
+  }
   return (
     <aside style={{background:'black',color:'skyblue',display:'flex',justifyContent:'center'}}>
       <h2>ai 2</h2>
+      <input value={prompt} onChange={(e)=>setPrompt(e.target.value)} style={{height:'2em'}}/>
       {responses?.map((item, i) => (
         <div key={i}> <p>{item.message.content}</p> </div>
       ))}
+      <button onClick={handleClick}>SEND</button>
     </aside>
   );
 }
